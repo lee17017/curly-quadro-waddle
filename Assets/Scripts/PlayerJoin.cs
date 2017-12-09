57ac730dc551b1f37d1c21bc8bad3634208e0f62
Robin Ostner <Ostner_Robin@web.de>
2017-12-09 12:42:28 +0100

    Main Menu

---
+++ Assets/Scripts/PlayerJoin.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerJoin : MonoBehaviour {
+
+    public int playerID;
+
+    private bool joined;
+
+    public Color joinedColor, notJoinedColor;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CheckStart();
+        SwitchJoined();
+        UpdateColor();
+    }
+
+    void UpdateColor()
+    {
+        if (joined)
+        {
+            spriteRenderer.color = joinedColor;
+        }
+        else
+        {
+            spriteRenderer.color = notJoinedColor;
+        }
+    }
+
+    void SwitchJoined()
+    {
+        if (InputManager.current.GetShoot("" + playerID))
+        {
+            joined = !joined;
+            switch (playerID)
+            {
+                case 1:
+                    Settings.p1 = joined;
+                    break;
+                case 2:
+                    Settings.p2 = joined;
+                    break;
+                case 3:
+                    Settings.p3 = joined;
+                    break;
+                case 4:
+                    Settings.p4 = joined;
+                    break;
+            }
+        }
+    }
+
+    void CheckStart()
+    {
+        switch (playerID)
+        {
+            case 1:
+                bool startpressed = Input.GetKeyDown(KeyCode.Joystick1Button9);
+
+                if (startpressed)
+                {
+                    StartGame();
+                }
+                break;
+            case 2:
+                startpressed = Input.GetKeyDown(KeyCode.Joystick2Button7);
+
+                if (startpressed)
+                {
+                    StartGame();
+                }
+                break;
+            case 3:
+                startpressed = Input.GetKeyDown(KeyCode.Joystick3Button7);
+
+                if (startpressed)
+                {
+                    StartGame();
+                }
+                break;
+            case 4:
+                startpressed = Input.GetKeyDown(KeyCode.Joystick4Button7);
+
+                if (startpressed)
+                {
+                    StartGame();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void StartGame()
+    {
+        if(Settings.p1 || Settings.p2 || Settings.p3 || Settings.p4)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
