c4869ce6b3ef6fc8bcfd82784804e49fc1121858
Mini <m.luong@hotmail.de>
2017-12-09 02:54:53 +0100

    Force not finished

---
+++ Assets/Scripts/GravityForce.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityForce : MonoBehaviour {
+
+    // public Rigidbody2D rb;
+    // public GameObject circle;
+    private float thrust;
+    private float radius;
+    private Vector2 center;
+
+
+    void Start()
+    {
+        center = transform.position;
+        radius = GetComponent<CircleCollider2D>().radius;
+        thrust = 0;
+
+        // rb = GetComponent<Rigidbody2D>();
+        Debug.Log("started");
+    }
+
+    void FixedUpdate()
+    {   
+        // Test if object inside a circle
+        // (x - center.x) ^ 2 + (y - center.y) ^ 2 < radius ^ 2;
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.left * Time.deltaTime);
+	}
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        GameObject colObj = collision.gameObject;
+        Debug.Log("collided");
+
+        Vector2 colPos = colObj.transform.position;
+        Vector2 forceVec = new Vector2(center.x - colPos.x, center.y - colPos.y);
+
+        Debug.Log("Force: " + forceVec);
+        thrust = forceVec.magnitude / 9;
+        colObj.GetComponent<Rigidbody2D>().AddForce(forceVec.normalized * thrust);
+        
+
+
+        // rb.AddForce(transform.up * thrust);
+    }
+
+
+
+
+
+}
